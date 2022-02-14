de9baae0ec7f1aa0d3fa47a20dad4c9b9b680f71
Patrick Todd <patrick.todd@hunterstrategy.net>
2022-02-13 22:05:13 -0500

    updated comments https

--- infra/alb.tf
+++ infra/alb.tf
@@ -5,7 +5,8 @@
  * Location: env/.tfvars
  *
  * Description:
- *  - Provisions application load balancer
+ *  - Provisions application load balancer, listener,
+ *    and target group
  * 
  * Author: Patrick Todd <patrick.a.todd@gmail.com>
  *
@@ -17,7 +18,7 @@ resource "aws_alb" "alb_graylog" {
   internal           = false
   load_balancer_type = "application"
 
-  security_groups = [aws_security_group.private_ag_graylog.id, aws_security_group.alb_sg_graylog.id]
+  security_groups = [aws_security_group.private_sg_graylog.id, aws_security_group.alb_sg_graylog.id]
   subnets         = module.aws_vpc.public_subnets
 
   enable_deletion_protection = false
@@ -25,28 +26,31 @@ resource "aws_alb" "alb_graylog" {
   tags = merge(local.tags, { Name = "alb-${local.name}" })
 }
 
-# ALB LISTENER - HTTP - PORT 8080
-resource "aws_alb_listener" "alb_listener_http_graylog" {
-  load_balancer_arn = aws_alb.alb_graylog.arn
-  port              = "8080"
-  protocol          = "HTTP"
+# ALB LISTENER - HTTPS
+resource "aws_alb_listener" "alb_listener_https_graylog" {
+  depends_on        = [aws_alb_target_group.tg_https_graylog]
 
+  load_balancer_arn = aws_alb.alb_graylog.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = var.certificate_arn
+   
   default_action {
-    target_group_arn = aws_alb_target_group.tg_graylog.arn
+    target_group_arn = aws_alb_target_group.tg_https_graylog.arn
     type             = "forward"
   }
 }
 
-# ALB TARGET GROUP - PORT 8080
-resource "aws_alb_target_group" "tg_graylog" {
-  name     = "tg-${local.name}"
-  port     = 8080
+# ALB TARGET GROUP - PORT 443
+resource "aws_alb_target_group" "tg_https_graylog" {
+  name     = "tg-https-${local.name}"
+  port     = 443
   protocol = "HTTP"
   vpc_id   = module.aws_vpc.vpc_id
 
   health_check {
     path                = "/"
-    port                = 8080
+    port                = 443
     healthy_threshold   = 10
     unhealthy_threshold = 10
     timeout             = 120
